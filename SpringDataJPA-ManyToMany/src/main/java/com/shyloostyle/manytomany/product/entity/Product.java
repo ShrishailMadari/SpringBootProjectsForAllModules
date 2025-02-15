fe123cc1529e294ce414321f7f404b1810fc001f
shrishail madari <shrishailmadari@gmail.com>
2025-02-15 10:32:42 +0530

    add these files

--- SpringDataJPA-ManyToMany/src/main/java/com/shyloostyle/manytomany/product/entity/Product.java
+++ SpringDataJPA-ManyToMany/src/main/java/com/shyloostyle/manytomany/product/entity/Product.java
@@ -1,8 +1,21 @@
 package com.shyloostyle.manytomany.product.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Product {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private double quality;
 }
--- SpringDataJPA-ManyToMany/src/main/java/com/shyloostyle/manytomany/user/entity/User.java
+++ SpringDataJPA-ManyToMany/src/main/java/com/shyloostyle/manytomany/user/entity/User.java
@@ -1,4 +1,23 @@
 package com.shyloostyle.manytomany.user.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String userName;
+    private String address;
+
 }
